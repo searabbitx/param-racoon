aab022694c84ec4e5e863fe787554588dbc198a4
searabbit <46087838+searabbitx@users.noreply.github.com>
2024-09-07 18:19:35 +0200

    Format main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "It seems to work!" << std::endl;
+int main() {
+  std::cout << "Hello." << std::endl;
+  std::cout << "It seems to work!" << std::endl;
 }
